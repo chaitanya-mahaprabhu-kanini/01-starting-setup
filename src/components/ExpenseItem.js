477384ca65771e32d4642daa170f2973eeeef0bf
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-06-08 11:26:11 +0530

    Added "Card" component with use of props.children.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,19 @@
 import "./ExpenseItem.css";
 import { ExpenseDate } from "./ExpenseDate";
+import {Card} from "./Card"
 function ExpenseItem(props) {
   const expenseDate = props.date;
   const expenseTitle = props.title;
   const expenseAmount = props.amount;
 
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={expenseDate}></ExpenseDate>
       <div className="expense-item__description">
         <h2>{expenseTitle}</h2>
         <div className="expense-item__price">{expenseAmount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
