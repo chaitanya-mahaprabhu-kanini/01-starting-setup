ef914acd8e16816ac3340fb32bb6dd7417d91aa0
chaitanya-mahaprabhu-kanini <chaitanya.kanini@gmail.com>
2023-06-11 16:40:31 +0530

    A chart feature to view the expenses in each year by month added. Along with the necessary components and css files.

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { Card } from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import { ExpensesList } from "./ExpensesList";
+import { ExpensesChart } from "./ExpensesChart";
 
 const Expenses = (props) => {
   const [filteredList, setFilteredList] = useState(
@@ -21,6 +22,7 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter onFilterChange={filterChangeHandler} />
+        <ExpensesChart expenses = {filteredList}/>
         <ExpensesList items={filteredList} />
       </Card>
     </div>
